49b0f919449bb95b1560107ac04c9edbbbb94f2f
EmilioPastor <emiliopastorzurita906@gmail.com>
2025-02-07 18:21:00 +0100

    Ejercicio 2

---
+++ src/lib/data.js
@@ -0,0 +1,81 @@
+'use server'
+
+import prisma from "@/lib/prisma"
+
+
+// ---------------------   REPARTIDORES -----------------------
+
+
+async function obtenerRepartidores() {
+    const repartidores = await prisma.repartidor.findMany()
+    return repartidores
+}
+
+
+
+
+async function obtenerRepartidor(id) {
+    const repartidor = await prisma.repartidor.findUnique({
+        where: { id: +id }
+    })
+    return repartidor
+}
+
+
+
+
+// ---------------------   PEDIDOS -----------------------
+
+
+async function obtenerPedidos() {
+    const pedidos = await prisma.pedido.findMany()
+    return pedidos
+}
+
+
+
+
+async function obtenerPedido(id) {
+    const pedido = await prisma.pedido.findUnique({
+        where: { id: +id },
+        include: {
+            repartidor: true,
+            pizzas: true,
+        }
+    })
+    return pedido
+}
+
+
+// ---------------------   PIZZAS -----------------------
+
+
+async function obtenerPizzas() {
+    const pizzas = await prisma.pizza.findMany({
+        include: {pedidos: true}
+    })
+    return pizzas
+}
+
+
+
+
+async function obtenerPizza(id) {
+    const pizza = await prisma.pizza.findUnique({
+        where: { id: +id },
+        include: {pedidos: true}
+    })
+    return pizza
+}
+
+
+
+
+export {
+    obtenerRepartidores,
+    obtenerRepartidor,
+    obtenerPedidos,
+    obtenerPedido,
+    obtenerPizzas,
+    obtenerPizza
+}
